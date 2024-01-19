fc3b00601ac88fae4b778a7bc311ccb72ff7c4ee
miguelfrancaa <miguelcrfranca@gmail.com>
2024-01-19 18:40:39 +0000

    First migrations done.

---
+++ database/migrations/2024_01_19_183340_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment');
+            $table->integer('rating');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign("user_id")->references('id')->on('users');
+
+            $table->unsignedBigInteger('recipe_id');
+            $table->foreign("recipe_id")->references('id')->on('recipes');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
